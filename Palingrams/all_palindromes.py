0b5d890c932d37f3f19310fc275727a1c63d91e7
OXXO <kulikov_stepan@sjtu.edu.cn>
2021-01-03 13:28:50 +0300

    remove function that loads dictionary, instead it is now called from load_dictionary.py

--- Palingrams/all_palindromes.py
+++ Palingrams/all_palindromes.py
@@ -1,28 +1,11 @@
 import sys
-# import load_dictionary
+import load_dictionary
 
 
-def load(file):
-    """Open a text file & return a list of lowercase strings."""
-
-    try:
-        with open(file) as in_file:
-            # remove whitespaces and put each word on a new line
-            loaded_txt = in_file.read().strip().split('\n')
-            # lowercase all the words
-            loaded_txt = [x.lower() for x in loaded_txt]
-            
-            return loaded_txt
-
-    except IOError as e:
-        print("{}\nErroropening{}.Terminatingprogram.".format(e,file), file=sys.stderr)
-        sys.exit(1)
-
-
-word_list = load('2of4brif.txt')
+word_list = load_dictionary.load('2of4brif.txt')
 pali_list = []
 
-
+# finding palindromes
 for word in word_list:
     if len(word) > 1 and word == word[::-1]:
         pali_list.append(word)

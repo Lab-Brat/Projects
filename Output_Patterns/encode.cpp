e4fbc79b08a2d6a9187059614d48a64d2b8fd1d8
Lab-Brat <xooVoox@protonmail.com>
2021-01-24 15:57:40 +0300

    input string is changed into something more intuitive

--- Output_Patterns/encode.cpp
+++ Output_Patterns/encode.cpp
@@ -3,20 +3,22 @@
 int main()
 {
 	int a,b=0,c=10;
-
-	const char *str=
-		"TFy!QJu ";
+	
+	// E(69)-64=5, str is a string of
+	// consecutive 5 ' 's and 5 '!'s
+	const char *str="@EEEEEEEEEEEEEEEEEEEE\
+			 EEEEEEEEEEEEEEEEEEEE";
 
 	do{
 		a=str[b];
-		b++;
+		b++; //1st char will always be ' ', 2nd '!' and so on
 
 		int len=a-64;   //execute loop a-64 times
 		while(len>0){
 			len--;
 			c++;        
 			char ch;
-			if(c==90){  //print \n on every 80th char
+			if(c==50){  //print \n on every 80th char
 				c=10;   
 				ch='\n';
 			}

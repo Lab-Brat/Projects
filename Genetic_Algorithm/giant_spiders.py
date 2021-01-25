8258130a49c51c2d2a564c800d28345c9b7f4b21
Lab-Brat <xooVoox@protonmail.com>
2021-01-25 15:07:59 +0300

    add selection procedure

--- Genetic_Algorithm/giant_spiders.py
+++ Genetic_Algorithm/giant_spiders.py
@@ -4,11 +4,11 @@
 import random
 
 # Constants
-GOAL = 10000
+GOAL = 6000
 NUM_SPI = 50
 INIT_MIN_W = 80
-INIT_MAX_W = 400
-INIT_MOD_W = 200
+INIT_MAX_W = 200
+INIT_MOD_W = 100
 MUTATE = 0.1
 MUTATE_MIN = 0.8
 MUTATE_MAX = 1.5
@@ -26,8 +26,25 @@ def mean(lis):
 def fitness(population, goal):
     return mean(population)/goal
 
-pop = populate(NUM_SPI, INIT_MIN_W, INIT_MAX_W, INIT_MOD_W)
-M = mean(pop)
-FIT = fitness(pop, GOAL)
+def select(population, to_retain):
+    pop_s = sorted(population)
+    retain_by_sex = to_retain//2
+    members_per_sex = len(pop_s)//2
 
-print(M)
+    # divide population by sex
+    males = pop_s[:members_per_sex] #   0->25 (from smallest w to mid)
+    females = pop_s[members_per_sex:] # 25->50 (form mid w to largest)
+    selected_males = males[-retain_by_sex:] 
+    selected_females = females[-retain_by_sex:] # select from end
+
+    # print(retain_by_sex, members_per_sex)
+
+    return selected_males, selected_females
+
+
+pop = populate(NUM_SPI, INIT_MIN_W, INIT_MAX_W, INIT_MOD_W)
+pop_fit = fitness(pop, GOAL)
+
+sm, sf = select(pop, NUM_SPI)
+
+print(sm, sf)

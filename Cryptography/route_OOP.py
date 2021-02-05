097d01eefded2e22eded440bada0a149dd08e928
Yara! <xooVoox@protonmail.com>
2021-02-05 23:56:42 +0300

    Added info processing method

--- Cryptography/route_OOP.py
+++ Cryptography/route_OOP.py
@@ -5,9 +5,15 @@ class RouteCipher:
         self.rows = rows
         self.key = key
 
+    def prep(self):
+        self.cipherlist = list(self.cipher.split())
+        self.key_int = [int(i) for i in self.key.split()]
+
     def __repr__(self):
-        return '[decryption info: {}, {}, {}]'.format(\
-                                 self.cols, self.rows, self.key)
+        # return '[decryption info: {}, {}, {}]'.format(\
+        #                          self.cols, self.rows, self.key)
+        return '[prep result: {}, {}]'.format(\
+                                   self.cipherlist, self.key_int)
 
 ciphertext = "16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19"
 COLS = 4
@@ -15,4 +21,5 @@ ROWS = 5
 KEY = "-1 2 -3 4"
 
 dec1 = RouteCipher(ciphertext, COLS, ROWS, KEY)
+dec1.prep()
 print(dec1)

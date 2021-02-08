c9c5fcf907a6b37222132dbcebb661c564ee2d41
Yara! <xooVoox@protonmail.com>
2021-02-08 11:56:49 +0300

    add decryption method

--- Cryptography/rail_OOP.py
+++ Cryptography/rail_OOP.py
@@ -1,4 +1,8 @@
 class RailFenceCipher:
+    """
+    Input: A string that you wish to encrypt or dencryption
+    Output: An encrypted or decrypted string
+    """
     def __init__(self, text):
         self.text = text
         self.split_text = [None] * 2
@@ -6,6 +10,8 @@ class RailFenceCipher:
         self.text_low = ("".join(self.text.split())).lower()
 
     def encrypt(self):
+        """Split the string in a zig-zag fashion, then put it back
+           together with spaces at every 5 character mark """
         upper_part = self.text_up[0::2]
         lower_part = self.text_up[1::2]
         result = upper_part + lower_part
@@ -13,11 +19,14 @@ class RailFenceCipher:
         return ' '.join([result[i:i+5] for i in range(0, len(result), 5)])
 
     def decrypt(self):
-        left_part = self.text_low[0:len(self.text_low)//2]
-        right_part = self.text_low[len(self.text_low)//2:len(self.text_low)]
+        """Split the string into two halves, then add a character 
+           from each string on every iterationv (reverse of zig-zag)"""
+        half = len(self.text_low)//2
+        left_part = self.text_low[0:half]
+        right_part = self.text_low[half:len(self.text_low)]
 
         result = ''
-        for i in range(len(self.text_low)//2):
+        for i in range(half):
             result += left_part[i]
             result += right_part[i]
 
@@ -35,5 +44,5 @@ if __name__ == "__main__":
     case1 = RailFenceCipher(plaintxt)
     case2 = RailFenceCipher(plaintxt2)
 
-    # print(case1.encrypt())
-    print(case2.decrypt())
+    print("After encryption:  {}".format(case1.encrypt()))
+    print("After dencryption: {}".format(case2.decrypt()))

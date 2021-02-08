fc077edf3ff7043cbc92239a7ffc2173c5dda355
Yara! <xooVoox@protonmail.com>
2021-02-08 11:35:01 +0300

    rearrange functions, add initialization to output lower case letters without spaces

--- Cryptography/rail_OOP.py
+++ Cryptography/rail_OOP.py
@@ -3,9 +3,7 @@ class RailFenceCipher:
         self.text = text
         self.split_text = [None] * 2
         self.text_up = ("".join(self.text.split())).upper()
-
-    def __repr__(self):
-        return "[Result: {}]".format(self.text_up)
+        self.text_low = ("".join(self.text.split())).lower()
 
     def encrypt(self):
         upper_part = self.text_up[0::2]
@@ -14,9 +12,19 @@ class RailFenceCipher:
 
         return ' '.join([result[i:i+5] for i in range(0, len(result), 5)])
 
+    # def decrypt(self):
+
+    def __repr__(self):
+        return "[Result: {}]".format(self.text_low)
+
 
 if __name__ == "__main__":
 
     plaintxt = 'Buy more Maine potatoes'
+    plaintxt2 = "BYOEA NPTTE UMRMI EOAOS"
+
     case1 = RailFenceCipher(plaintxt)
-    print(case1.encrypt())
+    case2 = RailFenceCipher(plaintxt2)
+
+    # print(case1.encrypt())
+    print(case2)

cbca9646f0ba0c1eef4945b8e4a55233e959ca20
Yara! <xooVoox@protonmail.com>
2021-02-12 18:53:04 +0300

    add put method, which recieves arguments that are stored in a dict

--- API/yt_flask/main.py
+++ API/yt_flask/main.py
@@ -1,16 +1,38 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 # initialize Flask application and an API
 app = Flask(__name__)
 api = Api(app)
 
-class TestString(Resource):
-    def get(self):
-        return {"data": "This is working just fine!"}
+# names = {"yara": {"age": 24, "gender": "male"},
+#          "vasya": {"age": 25, "gender": "female"}}
+
+# class TestString(Resource):
+#     def get(self, name):
+#         return names[name]
 
 # registering TestString as a resourse for testing
-api.add_resource(TestString, "/testing")
+# api.add_resource(TestString, "/testing/<string:name>")
+# <> defines a parameter to be passed in to Request
+
+video_args = reqparse.RequestParser()
+video_args.add_argument("name", type=str, help="Name of the video")
+video_args.add_argument("views", type=int, help="Views of the video")
+video_args.add_argument("likes", type=int, help="Likes of the video")
+
+videos = {2: {"name": "Vasya", "views": 3333333, "likes": 300}}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     # start server & Flask application
--- API/yt_flask/test.py
+++ API/yt_flask/test.py
@@ -3,5 +3,17 @@ import requests
 # defining base URL (location of an API)
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "testing")
+# response = requests.get(BASE + "testing/vasya")
+
+# testing video request
+response = requests.put(BASE + "video/1", {"name": "Yara", \
+                                           "views": 12345, \
+                                           "likes": 100})
 print(response.json())
+
+# pause until enter is pressed
+input()
+
+for i in range(2):
+    response = requests.get(BASE + "video/" + str(i+1))
+    print(response.json())

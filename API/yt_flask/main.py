a2ff1c2272d27ee6910ba9034f7f4efb75883df6
Yara! <xooVoox@protonmail.com>
2021-02-12 21:54:53 +0300

    cleaned and documented

--- API/yt_flask/main.py
+++ API/yt_flask/main.py
@@ -1,29 +1,24 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 # initialize Flask application and an API
 app = Flask(__name__)
 api = Api(app)
 
-# names = {"yara": {"age": 24, "gender": "male"},
-#          "vasya": {"age": 25, "gender": "female"}}
-
-# class TestString(Resource):
-#     def get(self, name):
-#         return names[name]
-
-# registering TestString as a resourse for testing
-# api.add_resource(TestString, "/testing/<string:name>")
-# <> defines a parameter to be passed in to Request
-
+# parse all needed arguments
 video_args = reqparse.RequestParser()
 video_args.add_argument("name", type=str, help="Name of the video")
 video_args.add_argument("views", type=int, help="Views of the video")
 video_args.add_argument("likes", type=int, help="Likes of the video")
 
-videos = {2: {"name": "Vasya", "views": 3333333, "likes": 300}}
+videos = {}
 
 class ID_Status():
+    """
+    Provide functionality to query whether video_id exists
+    in the database already or not.
+    """
     def no_id(self, vid_id):
         if vid_id not in videos:
             abort(404, message="video_id not found")
@@ -35,6 +30,15 @@ class ID_Status():
             abort(409, message="video_id already exists")
 
 class Video(Resource):
+    """
+    A Resource that handles 3 types of requests, namely:
+       1) get:    recieves video_id as input, checks if it
+                  exists in a videos or not, then outputs it;
+       2) put:    recieves video_id as input. parses arguments,
+                  then adds id and arguments to videos;
+       3) delete: recieves video_id as input, checks if it
+                  exists in a videos or not, then deletes it;
+    """
     def get(self, video_id):
         get_status = ID_Status()
         get_status.no_id(video_id)
@@ -51,6 +55,7 @@ class Video(Resource):
         del videos[video_id]
         return 'deletion successful', 204
 
+# registering Video as Resource
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
--- API/yt_flask/test.py
+++ API/yt_flask/test.py
@@ -3,36 +3,23 @@ import requests
 # defining base URL (location of an API)
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.get(BASE + "testing/vasya")
-
-# testing video request
-# response = requests.put(BASE + "video/1", {"name": "Yara", \
-#                                            "views": 12345, \
-#                                            "likes": 100})
-# print(response.json())
-#
-# # pause until enter is pressed
-# input()
-#
-# for i in range(2):
-#     response = requests.get(BASE + "video/" + str(i+1))
-#     print(response.json())
-
-# response = requests.get(BASE + "video/3")
-# print(response.json())
-
 data = [{"name": "Finding a job in IT", "views": 12345, "likes": 100},
         {"name": "How to cook bolognese", "views": 88888, "likes": 2000},
         {"name": "Otter eating sushi", "views": 9999999, "likes": 6000}]
 
+# put 3 videos into videos
 for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
 
+# delete 2nd video
 response = requests.delete(BASE + 'video/1')
 print(response)
+
+# wait, continue after pressing 'enter'
 input()
-# get the rest
+
+# output everything that's left
 for i in range(2):
     response = requests.get(BASE + "video/" + str(i+1))
     print(response.json())

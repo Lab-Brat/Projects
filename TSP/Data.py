9b8e26345520c41e263a7f21ee93dabb39fe7655
Yara! <xooVoox@protonmail.com>
2021-02-25 12:32:28 +0300

    OOP version, added some generalization

--- TSP/Data.py
+++ TSP/Data.py
@@ -14,19 +14,31 @@ data = [        [10,10], [15,30], [45,77],  [20,50],  [60,50],
                 [76,67], [57,50], [99,21],  [99,88],  [5,31]
         ]
 
-
-ran_seq = random.sample(range(1,41), 40)
-
 res1 = [5,10,15,20,25,30,35,40]     # only visit 3 of these
 res2 = [2,4,6,8,10,20,22,32,33,35]  # only visit 5 of these
 
-res1_sel = random.sample(res1, 3)
-res2_sel = random.sample(res2, 5)
+class dataPreProcess():
+    def __init__(self, data, res1, res2):
+        self.data = data
+        self.ran_seq = random.sample(range(1,len(data)+1), len(data))
+        self.res1 = res1
+        self.res2 = res2
 
-res_set = set(res1).union(set(res2))
-res_sel_set = set(res1_sel).union(set(res2_sel))
+    def getLocs(self):
+        res1_sel = random.sample(self.res1, 3)
+        res2_sel = random.sample(self.res2, 5)
 
-fin_seq = [i for i in ran_seq if i not in res_set]
-fin_seq.extend(res_sel_set)
+        res_set = set(res1).union(set(res2))
+        res_sel_set = set(res1_sel).union(set(res2_sel))
 
-print(sorted(fin_seq), len(fin_seq))
+        fin_seq = [i for i in self.ran_seq if i not in res_set]
+        fin_seq.extend(res_sel_set)
+
+        return fin_seq
+
+
+DPP = dataPreProcess(data, res1, res2)
+FS = DPP.getLocs()
+
+print(FS, len(FS))
+print(len(data))

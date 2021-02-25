9f40208b546b2e14daa7bbb73aba844a200d412f
Yara! <xooVoox@protonmail.com>
2021-02-25 12:18:13 +0300

    heavily simplified coords_indx_res with the use of list comprehensions and sets

--- TSP/Data.py
+++ TSP/Data.py
@@ -13,9 +13,20 @@ data = [        [10,10], [15,30], [45,77],  [20,50],  [60,50],
                 [11,54], [11,90], [11,18],  [80,43],  [43,81],
                 [76,67], [57,50], [99,21],  [99,88],  [5,31]
         ]
-        
 
-ran_seq = [i for i in range(1,40)]
-random.shuffle(ran_seq)
 
-print(ran_seq)
+ran_seq = random.sample(range(1,41), 40)
+
+res1 = [5,10,15,20,25,30,35,40]     # only visit 3 of these
+res2 = [2,4,6,8,10,20,22,32,33,35]  # only visit 5 of these
+
+res1_sel = random.sample(res1, 3)
+res2_sel = random.sample(res2, 5)
+
+res_set = set(res1).union(set(res2))
+res_sel_set = set(res1_sel).union(set(res2_sel))
+
+fin_seq = [i for i in ran_seq if i not in res_set]
+fin_seq.extend(res_sel_set)
+
+print(sorted(fin_seq), len(fin_seq))

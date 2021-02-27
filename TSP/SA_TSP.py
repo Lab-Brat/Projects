f67a0d8120b6f492ec6c8be36c8769038e210af2
Yara! <xooVoox@protonmail.com>
2021-02-27 11:06:41 +0300

    scraped everything, started from scratch. Added 2 functions to calculate distance and fitness

--- TSP/SA_TSP.py
+++ TSP/SA_TSP.py
@@ -1,50 +1,24 @@
-import random, math, copy, numpy
+import random, math
+import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt
-from scipy.spatial import distance
+from scipy.spatial.distance import euclidean as eu
 from Data import dataPreProcess
 
-# Importing Data
-coords_list = dataPreProcess().getLocs()
 
-############################## SIMULATED ANNEALING #######################################
+coords = dataPreProcess().getLocs()
+path = [i for i in range(len(coords))]
+random.shuffle(path)
 
-# DEFINING THE ALGORITHM
-def TSP_SA(data,n):
+def dist(loc1, loc2):
+    """ Euclidean distance between two nodes. """
+    return eu(coords[loc1], coords[loc2])
 
-    customers = data.copy()
-    #choosing random route
-    route = random.sample(range(n),n)
+def fitness(path):
+    """ Total distance of the current path. """
+    fit = 0
+    for i in range(len(coords)):
+        fit += dist(path[i], path[(i+1)%len(coords)])
+    return fit
 
-    #THE ALGORITHM
-    #setting up temperature, 200K in this case
-    for temperature in numpy.logspace(0,5,num=200000)[::-1]:
-        #make a new route by randomly swapping two customers in 'route'
-        [i,j] = sorted(random.sample(range(n),2))
-        new_Route =  route[:i] + route[j:j+1] +  route[i+1:j] + route[i:i+1] + route[j+1:]
-
-        #fourmulas
-        oldDistances =  sum([ math.sqrt(sum([(customers[route[(k+1) % n]][d] - customers[route[k % n]][d])**2 for d in [0,1] ])) for k in [j,j-1,i,i-1]])
-        newDistances =  sum([ math.sqrt(sum([(customers[new_Route[(k+1) % n]][d] - customers[new_Route[k % n]][d])**2 for d in [0,1] ])) for k in [j,j-1,i,i-1]])
-
-        #if if condition is satisfied, old route will be replace by new route
-        if math.exp( ( oldDistances - newDistances) / temperature) > random.random():
-            route = copy.copy(new_Route)
-
-
-    #setting up parameters to plot the result
-    xs = [customers[route[i % n]][0] for i in range(n+1)]
-    ys = [customers[route[i % n]][1] for i in range(n+1)]
-
-    total_dist = 0
-    for k in range(len(xs)-1):
-        total_dist += sqrt((xs[k] - xs[k+1])**2 + (ys[k] - ys[k+1])**2)
-
-    plt.plot(xs, ys, 'ob-')
-    plt.show()
-
-    print("Total Distance: ", total_dist)
-    print(customers)
-
-
-TSP_SA(coords_list,len(coords_list))
+fit = fitness(path)
+print(fit)

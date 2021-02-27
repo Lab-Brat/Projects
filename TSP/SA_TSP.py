d9e09a706eae1b7a3e8ec25046b704d59eeae13c
Yara! <xooVoox@protonmail.com>
2021-02-27 12:02:34 +0300

    OOP implementation

--- TSP/SA_TSP.py
+++ TSP/SA_TSP.py
@@ -2,37 +2,39 @@ import random
 from scipy.spatial.distance import euclidean as eu
 from Data import dataPreProcess
 
+class SA():
+    def __init__(self):
+        self.coords = dataPreProcess().getLocs()
+        self.L = len(self.coords)
+        self.all_locs = [i for i in range(self.L)]
 
-coords = dataPreProcess().getLocs()
+    def dist(self, loc1, loc2):
+        """ Euclidean distance between two locations. """
+        return eu(self.coords[loc1], self.coords[loc2])
 
-def dist(loc1, loc2):
-    """ Euclidean distance between two locations. """
-    return eu(coords[loc1], coords[loc2])
+    def fitness(self, path):
+        """ Fitness value (total distance) of the path. """
+        fit = 0
+        for i in range(self.L):
+            fit += self.dist(path[i], path[(i+1)%self.L])
+        return fit
 
-def fitness(path):
-    """ Fitness value (total distance) of the path. """
-    fit = 0
-    for i in range(len(coords)):
-        fit += dist(path[i], path[(i+1)%len(coords)])
-    return fit
+    def greedy(self):
+        """ Greedy algorithm to obtain an initial path. """
+        visit_loc = random.choice(self.all_locs)
+        path = [visit_loc]
+        unvisit_loc = set(self.all_locs)
+        unvisit_loc.remove(visit_loc)
 
+        # find shortest path
+        while unvisit_loc:
+            next_loc = min(unvisit_loc, key=lambda x: self.dist(visit_loc, x))
+            unvisit_loc.remove(next_loc)
+            path.append(next_loc)
+            visit_loc = next_loc
 
-def greedy():
-    """ Greedy algorithm to obtain an initial path. """
-    all_locs = [i for i in range(len(coords))]
-    visit_loc = random.choice(all_locs)
-    path = [visit_loc]
-    unvisit_loc = set(all_locs)
-    unvisit_loc.remove(visit_loc)
+        return path, self.fitness(path)
 
-    # find shortest path
-    while unvisit_loc:
-        next_loc = min(unvisit_loc, key=lambda x: dist(visit_loc, x))
-        unvisit_loc.remove(next_loc)
-        path.append(next_loc)
-        visit_loc = next_loc
-
-    return path, fitness(path)
-
-fin_path, fit = greedy()
-print("Path: {0}\nTotal distance: {1:.2f}".format(fin_path, fit))
+if __name__ == "__main__":
+    fin_path, fit = SA().greedy()
+    print("Path:\n{0}\nTotal distance: {1:.2f}".format(fin_path, fit))

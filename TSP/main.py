4959775fb93dabe40a2f174141070dbf930a04fe
Yara! <xooVoox@protonmail.com>
2021-03-02 10:13:08 +0300

    added time measurements

--- TSP/main.py
+++ TSP/main.py
@@ -1,3 +1,4 @@
+import time
 from Data import dataPreProcess
 from GA_TSP import GA
 from SA_TSP import SA
@@ -5,10 +6,16 @@ from SA_TSP import SA
 coords = dataPreProcess().getLocs()
 
 print("---------  GA Starts  ---------")
+start = time.time()
 GA = GA(coords, 250)
 path_ga, path_fit_ga = GA.genetic()
-print("GA Total Distance: ", path_fit_ga)
+end = time.time()
+print("GA Total Distance: {0:.2f}, Time: {1:.2f}s".format(\
+                                  path_fit_ga, end-start))
 
+start = time.time()
 SA = SA(coords)
 path_sa, path_fit_sa = SA.sim_anneal()
-print("SA Total Distance: ", path_fit_sa)
+end = time.time()
+print("SA Total Distance: {0:.2f}, Time: {1:.2f}s".format(\
+                                  path_fit_sa, end-start))

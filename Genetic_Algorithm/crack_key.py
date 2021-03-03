21b2a8ab86285df931cf2219d0881f18591383a1
Yara! <xooVoox@protonmail.com>
2021-03-03 16:19:27 +0300

    fixed bug in brute_force; small changes in genetic_hill

--- Genetic_Algorithm/crack_key.py
+++ Genetic_Algorithm/crack_key.py
@@ -8,15 +8,15 @@ class Crack_Code():
         self.code_ints = code_ints
         self.combo = [random.choice(code_ints) for i in range(code_len)]
         self.N = len(self.combo)
-        self.nums = [i for i in range(10)]
 
     def brute_force(self):
         count = 0
-        permutations = product(self.nums, repeat=self.N)
+        permutations = product(self.code_ints, repeat=code_len)
+        print(self.combo)
 
         # find permutations with repetition to guess
         for p in permutations:
-            if p == self.combo:
+            if list(p) == self.combo:
                 print("Cracked!!!")
                 print("The code was: {}".format(self.combo))
                 print("It took {} tries".format(count))
@@ -34,21 +34,18 @@ class Crack_Code():
         count = 0
         attempt = [0]*self.N
         attempt_fit = self.fitness(attempt)
-        print(self.combo)
 
         while attempt != self.combo:
             next_att = attempt[:]
             find_num = random.randint(0, self.N-1)
-            next_att[find_num] = random.sample(self.nums,1)[0]
+            next_att[find_num] = random.choice(self.code_ints)
 
             next_att_fit = self.fitness(next_att)
             if next_att_fit > attempt_fit:
                 attempt = next_att[:]
                 attempt_fit = next_att_fit
-            print(next_att, attempt)
             count += 1
 
-        print()
         print("Cracked!!!")
         print("The code was: {}".format(self.combo))
         print("It took {} tries".format(count))
@@ -57,10 +54,11 @@ class Crack_Code():
 if __name__ == '__main__':
     start = time.time()
 
-    # combo_t = tuple([random.randint(0,9) for i in range(10)])
-    code_len = 10
-    code_ints = [i for i in range(10)]
-    Crack_Code(code_len, code_ints).genetic_hill()
+    code_len = 7
+    code_ints = [i for i in range(code_len)]
+    model = Crack_Code(code_len, code_ints)
+    # model.genetic_hill()
+    model.brute_force()
 
     end = time.time()
     print("Runtime was {0:.2f} seconds.".format(end-start))

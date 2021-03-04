2c22f048afcb0109a0d0ef51743a3c7fa89bdaf3
Yara! <xooVoox@protonmail.com>
2021-03-04 21:29:28 +0300

    locations are now read from locations.txt

--- TSP/Data.py
+++ TSP/Data.py
@@ -1,30 +1,19 @@
 import random
 import matplotlib.pyplot as plt
 
-# with open("locations.txt") as txt:
-#     data = [loc for loc in txt]
-#     txt.close()
+coords = []
+with open("locations.txt") as txt:
+    for line in txt.readlines():
+        line = [int(x.replace("\n", "")) for x in line.split(" ")]
+        coords.append(line)
 
-# custromers locations
-data = [        [10,10], [15,30], [45,77],  [20,50],  [60,50],
-                [60,15], [90,25], [90,79],  [54,18],  [21,10],
-                [63,91], [16,71], [26,29],  [58,39],  [33,15],
-                [33,55], [80,90], [100,79], [100,59], [22,80],
-                [33,81], [48,88], [50,77],  [90,76],  [20,56],
-                [80,60], [30,5],  [20,10],  [0,30],   [100,80],
-                [11,54], [11,90], [11,18],  [80,43],  [43,81],
-                [76,67], [57,50], [99,21],  [99,88],  [5,31]
-        ]
-
-# visiting restrictions
 res1 = [5,10,15,20,25,30,35,40]     # only visit 3 of these
 res2 = [2,4,6,8,10,20,22,32,33,35]  # only visit 5 of these
 
-
 class dataPreProcess():
     def __init__(self):
-        self.data = data
-        self.ran_seq = random.sample(range(1,len(data)+1), len(data))
+        self.data = coords
+        self.ran_seq = random.sample(range(1,len(self.data)+1), len(self.data))
         self.res1 = res1
         self.res2 = res2
 
@@ -73,7 +62,3 @@ class dataPreProcess():
         axs[0].plot(xs1, ys1, 'ob-')
         axs[1].plot(xs2, ys2, 'om-')
         plt.show()
-
-if __name__ == '__main__':
-    LOC = dataPreProcess().getLocs()
-    print(LOC)

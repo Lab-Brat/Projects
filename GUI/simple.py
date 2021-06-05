0637408ac919722945ebbfc3b3aed47850c9ce41
Yara <xooVoox@protonmail.com>
2021-06-05 17:11:34 +0300

    add button that runs script

--- GUI/simple.py
+++ GUI/simple.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 
@@ -12,23 +13,15 @@ class gui():
     def mid_screen(self, resize=True):
         screen_wid = self.root.winfo_screenwidth()
         screen_hei = self.root.winfo_screenheight()
-        #find center
         center_x = int(screen_wid/2 - self.width/2)
         center_y = int(screen_hei/2 - self.height/2)
         self.root.geometry(f'{self.width}x{self.height}+{center_x}+{center_y}')
         if resize==False:
             self.root.resizable(False,False)
 
-    def return_pressed(self, event):
-        print('Return key pressed.')
-
-    def log(self, event):
-        print(event)
-
     def button(self):
-        btn = ttk.Button(self.root, text='Save')
-        btn.bind('<Return>', self.return_pressed)
-        btn.bind('<Return>', self.log, add='+')
+        btn = tk.Button(self.root, text='Run Script')
+        btn['command'] = lambda: os.system('./run.sh')
         btn.focus()
         btn.pack(expand=True)
 
@@ -38,12 +31,12 @@ class gui():
         else:
             self.root.geometry(self.width, self.height)
 
-        #message = tk.Label(self.root, text=m_text).pack()
+        message = tk.Label(self.root, text=m_text).pack()
         self.button()
         self.root.mainloop()
 
 
 if __name__ == "__main__":
     title = 'WINDOW'
-    m_text = 'This is a window!'
+    m_text = 'run bash script!'
     gui(title).show(m_text, 'mid')
